ffca0cfad906e19036b0a72a7af3a3d4d808b5f0
Nate Vack <njvack@wisc.edu>
2015-06-22 12:21:28 -0500

    typo

--- voitools/voi.py
+++ voitools/voi.py
@@ -2,7 +2,7 @@
 """
 Part of the voitools package
 Copyright 2015 Board of Regents of the University of Wisconsin System
-Licensed under the MIT license; see LICENSE at the root fo the package.
+Licensed under the MIT license; see LICENSE at the root of the package.
 
 Authored by Nate Vack <njvack@wisc.edu> at the Waisman Laboratory for Brain
 Imaging and Behavior.
